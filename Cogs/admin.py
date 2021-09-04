1ef936cf90f561be8c497c59cb74110b552f0946
Fear <42700057+FearOfficial@users.noreply.github.com>
2021-09-03 21:44:54 -0400

    Update admin.py

--- Cogs/admin.py
+++ Cogs/admin.py
@@ -75,11 +75,11 @@ class Admin(commands.Cog, name='admin', command_attrs=dict(hidden=True)):
 
     @commands.Cog.listener()
     async def on_guild_join(self, guild: discord.Guild):
-        if "nigger" in guild.name.lower():
+        if "N Word" in guild.name.lower():
             await self.try_to_send_msg_in_a_channel(guild, "im gonna leave cuz of the server name")
             return await guild.leave()
         for channel in guild.channels:
-            if "nigger" in channel.name.lower():
+            if "N Word" in channel.name.lower():
                 await self.try_to_send_msg_in_a_channel(guild, f"im gonna leave cuz of the channel name {channel.mention}")
                 return await guild.leave()
 
